cf8b70296858f93cf0185766b13b804ee7bdc576
Std@0341 <rahul.shome@21north.in>
2020-04-03 17:37:10 +0530

    Test

---
+++ precache-manifest.eef99572a8d6728f400f416c3818c05a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e6de27583914582c310bd9518a7753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0c14ab6f526d54489a7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77ca36b6540365346f1d",
+    "url": "/static/js/2.2fdb2cbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2fdb2cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c14ab6f526d54489a7",
+    "url": "/static/js/main.d9243bee.chunk.js"
+  },
+  {
+    "revision": "841e990973483c3df0f4",
+    "url": "/static/js/runtime-main.0fef364f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
